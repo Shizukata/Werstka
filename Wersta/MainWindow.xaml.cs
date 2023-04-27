fcc30524165232ed4fa9c1579463ed08479ef134
Shizukata <freepixel007@gmail.com>
2023-04-27 12:34:40 +0300

    0.1

---
+++ Wersta/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wersta
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User(1, "Ivan", "Ivanov", 19, "Kazan"));
+            users.Add(new User(2, "Albert", "Biven", 5, "Kazan"));
+            users.Add(new User(3, "Kirill", "Izvini", 29, "Kazan"));
+            users.Add(new User(4, "Ivan", "Galunov", 129, "Kazan"));
+            LvList.ItemsSource = users;
+        }
+    }
+}
---
+++ Wersta/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wersta
+{
+    public class User
+    {
+        private object value;
+
+        public User(int Id, string Name, string SecondName, int Age, string City)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.SecondName = SecondName;
+            this.Age = Age;
+            this.City = City;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
